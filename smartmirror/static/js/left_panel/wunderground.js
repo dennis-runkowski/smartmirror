bc81e7b98677acb9359ff0d7275831dc8c1798ea
denrun-p <dennis.runkowski28@gmail.com>
2018-12-01 23:19:19 -0500

    install patch

--- smartmirror/static/js/left_panel/wunderground.js
+++ smartmirror/static/js/left_panel/wunderground.js
@@ -18,17 +18,20 @@ callOnInterval(360000, '/left_panel',
        var for_icon;
        var icon = data["current"]["link"];
        var current_temp = data["current"]["temp_f"];
-       $("#weather_icon").attr('src', icon)
-       $(current_weather).html(current_temp + " &#8457;")
-       $(weather_table).html("");
-       $.each( wundergroundData["forecast"], function( index, value ){
-            condition = value["condition"]
-            temp = value["temp"]
-            time = value["time"]
-            for_icon = value["link"]
-            tableData = "<th><div>" + time + "</div><div>" + temp + " &#8457;</div><div>" + condition + "</div><img id='forecast_icon' src=" + for_icon +"></th>" 
-            $(weather_table).append(tableData);
-
-        });
+       if (current_temp !== "Null") {
+            $("#weather_icon").attr('src', icon)
+            $(current_weather).html(current_temp + " &#8457;")
+       }
+       if (wundergroundData["forecast"][0]["time"] !== "Error") {
+            $(weather_table).html("");
+            $.each( wundergroundData["forecast"], function( index, value ){
+                condition = value["condition"]
+                temp = value["temp"]
+                time = value["time"]
+                for_icon = value["link"]
+                tableData = "<th><div>" + time + "</div><div>" + temp + " &#8457;</div><div>" + condition + "</div><img id='forecast_icon' src=" + for_icon +"></th>"
+                $(weather_table).append(tableData);
+            });
+       };
        
 });
